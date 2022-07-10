f7a58cf9aca4038f90b12cb9047eccad5066cec2
isaacd <isaacdarzi@gmail.com>
2022-07-10 15:00:21 -0400

    import security

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,43 @@
 const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
 const app = express();
 
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if (!isProduction) {
+   // enable cors only in development
+   app.use(cors());
+ }
+ 
+ // helmet helps set a variety of headers to better secure your app
+ app.use(
+   helmet.crossOriginResourcePolicy({ 
+     policy: "cross-origin" 
+   })
+ );
+ 
+ // Set the _csrf token and create req.csrfToken method
+ app.use(
+   csurf({
+     cookie: {
+       secure: isProduction,
+       sameSite: isProduction && "Lax",
+       httpOnly: true
+     }
+   })
+ );
 // const booksRouter = require('./routes/Bookings');
 // app.use('/Bookings', booksRouter);
 // const reviewsRouter = require('./routes/Reviews');
@@ -11,6 +48,10 @@ const app = express();
 // app.use('/Users', usersRouter);
 
 app.use(express.json())
+
+const routes = require('./routes');
+app.use(routes);
+
 app.get('/', function(req, res){
    //Create an error and pass it to the next function
    let err = new Error("Something went wrong");
