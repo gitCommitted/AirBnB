6b961f99b6ea59a97175207093b204490058903c
isaacd <isaacdarzi@gmail.com>
2022-07-13 20:58:11 -0400

    phase3 complete

--- backend/Routes/api/index.js
+++ backend/Routes/api/index.js
@@ -3,7 +3,45 @@ const router = require('express').Router();
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
-  
+
+// backend/routes/api/index.js
+// test set token cookie
+
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
+//test restorUser
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+//test requireAuth
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
 
 
 
