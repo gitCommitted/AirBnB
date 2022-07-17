3f398a952fa9c52fd086165a7a0e3431680b64d4
isaacd <isaacdarzi@gmail.com>
2022-07-17 09:37:49 -0400

    heroku troubleshooting

--- backend/app.js
+++ backend/app.js
@@ -39,51 +39,13 @@ if (!isProduction) {
      }
    })
  );
-// const booksRouter = require('./routes/Bookings');
-// app.use('/Bookings', booksRouter);
-// const reviewsRouter = require('./routes/Reviews');
-// app.use('/Reviews', reviewsRouter);
-// const spotsRouter = require('./routes/Spots');
-// app.use('/Spots', spotsRouter);
-// const usersRouter = require('./routes/users');
-// app.use('/Users', usersRouter);
 
-app.use(express.json())
+
 
 //connect all routes
 
 app.use(routes);
 
-// app.get('/', function(req, res){
-//    //Create an error and pass it to the next function
-//    let err = new Error("Something went wrong");
-//    err.statusCode=403
-//    console.log(err.statusCode)
-//    next(err);
-// });
-
-/*
- * other route handlers and middleware here
- * ....
- */
-
-//An error handling middleware
-// app.use((err, req, res, next)=>{
-//    if (err.status===403){
-//        res.json({
-//         "message": "Forbidden",
-//         "statusCode": 403
-//        })
-//     }
-//    if (err.statusCode===403){
-//         res.json({
-//         "message": "Authentication Required",
-//         "statusCode": 401
-//         })
-//     }
-//     console.log(err.statusCode)
-//     res.json({statusCode: err.statusCode})
-// });
 
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
@@ -118,7 +80,4 @@ app.use((err, _req, res, _next) => {
    });
  });
 
-
-// const port = 3000;
-// app.listen(port, () => console.log('Server is listening on port', port));
 module.exports = app;
