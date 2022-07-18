ff2585bb4679137136c12a96347a9c504887a772
isaacd <isaacdarzi@gmail.com>
2022-07-18 19:49:46 -0400

    rename back

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const routes = require('./apiroutes');
+const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
