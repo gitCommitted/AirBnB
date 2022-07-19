13982496f5a539cfeff0ece4a87f4513fa3fec3b
isaacd <isaacdarzi@gmail.com>
2022-07-19 19:32:05 -0400

    finished signup and login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -74,14 +74,46 @@ router.get(
 // Sign up: /signup
 router.post(
   '/signup',
-  async (req, res) => {
+  async (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
+    if (!email || !firstName || !lastName){
+      const err = new Error('Validation Failed');
+      err.status = 400;
+      err.title = 'Validation Failed';
+      err.errors = {
+        "email": "Invalid email",
+        "firstName": "First Name is required",
+        "lastName": "Last Name is required"
+      };
+      return next(err);
+    }
+    
+    const tryAddy = await User.findOne({
+      where : {
+        email: email
+      }
+    })
+    if (tryAddy){
+      const err = new Error('User Already Exists');
+        err.status = 403;
+        err.title = 'User Already Exists';
+        err.errors = {
+          "email": "User with that email already exists"
+        };
+        return next(err);
+    }
+
+
     const user = await User.signup({ firstName, lastName, email, password });
 
-    await setTokenCookie(res, user);
+    const token=await setTokenCookie(res, user);
 
-    return res.json({
-      user
+    return res.json( {
+      "id": user.id,
+      "firstName": user.firstName,
+      "lastName": user.lastName,
+      "email": user.email,
+      "token": token
     });
   }
 );
