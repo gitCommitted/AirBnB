d99d95128231d842e7e3afe3939a9f6fb82fc9c4
isaacd <isaacdarzi@gmail.com>
2022-07-20 20:14:13 -0400

    added 1 spot route

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { User,Spot } = require('../../db/models');
+
+const router = express.Router();
+
+//get all spots
+router.get(
+    '/',
+    async (req, res) => {
+      
+     const Spots = await Spot.findAll({
+        
+    });
+      return res.json({Spots});
+    }
+  );
+
+
+
+
+
+
+
+module.exports = router;
