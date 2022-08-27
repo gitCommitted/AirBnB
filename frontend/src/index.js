45b6a1b3550a1622bb300bd3ba0277b083def7b2
isaacd <isaacdarzi@gmail.com>
2022-08-27 15:00:56 -0400

    in the middle of authentication setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
-import configureStore from './store';
+import configureStore from './store/index';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
